8a462b590fb2a71a94b4d4a5354900be426b1769
Costa202 <gabrielclopess@hotmail.com>
2022-03-08 20:13:34 -0300

    new change

--- myProject/Adivinhe.py
+++ myProject/Adivinhe.py
@@ -1,1 +1,41 @@
-print("oi git")
+#Chute um número
+# objetivo criar um algoritimo que gera um valor aleatório e eu tenho que ficar tentando acertar o número (projeto com interface)
+
+import random
+
+class ChuteONumero:
+    def __init__(self):
+        self.valor_aleatorio = 0
+        self.valor_minimo = 1
+        self.valor_maximo = 100
+        self.tentar_novamente = True
+
+    def Iniciar(self):
+        self.GerarNumeroAleatorio()
+        self.PedirValorAleatorio()
+        try:
+            while self.tentar_novamente == True:
+                if int(self.valor_do_chute) > self.valor_aleatorio:
+                    print("Chute um valor mais BAIXO!")
+                    self.PedirValorAleatorio()
+                elif int(self.valor_do_chute) < self.valor_aleatorio:
+                    print("Chute um valor mais ALTO!")
+                    self.PedirValorAleatorio()
+                elif int(self.valor_do_chute) == self.valor_aleatorio:
+                    self.tentar_novamente = False
+                    print('PARABÉNS você acertou!!')
+        
+        except:
+            print("Favor digitar apenas números")
+            self.Iniciar()
+    
+
+
+    def PedirValorAleatorio(self):
+        self.valor_do_chute = input("Chute um número:")
+
+    def GerarNumeroAleatorio(self):
+       self.valor_aleatorio = random.randint(self.valor_minimo,self.valor_maximo)
+
+chute = ChuteONumero()
+chute.Iniciar()
